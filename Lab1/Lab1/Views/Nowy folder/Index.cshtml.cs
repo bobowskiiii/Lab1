379ad35e18421284e3a1c4789199ee8f9741ef67
Szymon Bobowski <szymon.bobowski@microsoft.wsei.edu.pl>
2025-10-05 18:55:33 +0200

    added 3 new files
    added 3 new files

---
+++ Lab1/Lab1/Views/Nowy folder/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab1.Views.Nowy_folder
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
